fea2a17479459a257b102c09c9ac2666d2814724
thiago <thiago_arruda@id.uff.br>
2023-07-09 03:28:12 -0300

    Update AddButton.tsx

--- src/components/calculator/assets/AddButton.tsx
+++ src/components/calculator/assets/AddButton.tsx
@@ -1,6 +1,6 @@
 /* styles import */
 import "../../../styles/main.sass";
-import "../../../styles/components/calculator/assets/add_button.sass"
+import "../../../styles/components/calculator/assets/add_button.sass";
 import { useContext } from "react";
 import { ClientContext } from "../contexts/ClientContext";
 import { GlobalContext } from "../contexts/GlobalContext";
@@ -13,20 +13,27 @@ const AddButton = (props: AddButtonProps) => {
   const { fatores } = useContext(GlobalContext);
   const { data, setData } = useContext(ClientContext);
 
+  const calcularNovoValor = () => {
+    const mes = (data.total.mes + props.value) / 1000;
+    const arvores = fatores
+      ? data.total.arvores + props.value * fatores?.conversaoArvores
+      : data.total.arvores;
+    const ano = mes * 12;
+
+    setData({
+      ...data,
+      total: {
+        mes: mes,
+        arvores: arvores,
+        ano: ano,
+      },
+    });    
+  };
+
   return (
     <button
       className="add__button"
-      onClick={(e) => {
-        setData(fatores ? {
-          ...data,
-          total: {
-            mes: (data.total.mes + props.value) / 1000,
-            ano: (data.total.ano + props.value * 12) / 1000,
-            arvores:
-              (data.total.mes + props.value) * fatores?.conversaoArvores,
-          },
-        } : data);
-      }}
+      onClick={(e) => calcularNovoValor()}
     >
       Adicionar ao cálculo
     </button>
